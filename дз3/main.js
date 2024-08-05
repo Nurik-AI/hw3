8a04ff92efcbab9a797fcab0af5094fff9cb0d59
Nurik-AI <zulpuevnursultan1@gmail.com>
2024-08-05 18:32:11 +0600

    Add files via upload

---
+++ дз3/main.js
@@ -0,0 +1,24 @@
+// function square(number) {
+//     return number * number;
+//   }
+  
+//   const result = square(3);
+//   console.log(result); 
+  
+
+//   const double = number => number * 2;
+
+//   const result = double(5);
+//   console.log(result); 
+  
+// доп.
+
+const book = {
+    title: "48 законов",
+    author: "Роберт Грин",
+    year: 1998
+  };
+  
+  console.log("Название:", book.title); 
+  console.log("Автор:", book.author);   
+  console.log("Год выпуска:", book.year);
